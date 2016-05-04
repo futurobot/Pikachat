c871da4ded72aa2526291fda3ed2b77e2841ad2b
Alexey Kalyaganov <aa.kalyaganov@yandex.ru>
2016-05-04 07:45:14 +0300

    Add boobs handler

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Pikabu chat bot
 import logging
 
-import random
 import config
+import random
+import re
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 
 # Enable logging
@@ -15,7 +15,12 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 TIME_TO_REPOST = dict()
-fochi_replyes = ["Ко ко ко", "Воу воу, палехчи паринь", "Смотрите все! Fochi бушует!", "Слушайте его, он херни не скажет", "Круто сказанул, ыыыыы"]
+fochi_replyes = ["Ко ко ко", "Воу воу, палехчи паринь", "Смотрите все! Fochi бушует!",
+                 "Слушайте его, он херни не скажет", "Круто сказанул, ыыыыы"]
+
+boobs_channels = ['@superboobs', '@boobsChannel', '@boobsblog', '@BestTits']
+boobs_regexp = re.compile('(сис(ек|ьки|ечки|и|яндры))|(ти(тьки|течки|тюли|ти))')
+
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
@@ -28,21 +33,17 @@ def help(bot, update):
 
 
 def echo(bot, update):
-    username = update.message.from_user.username
-    last_name = update.message.chat.last_name
-    if username == "fochi_ip":
+    if boobs_regexp.match(update.message.text) is not None:
+        bot.forwardMessage(chat_id=update.message.chat_id,
+                           from_chat_id=boobs_channels[random.randint(0, len(boobs_channels) - 1)],
+                           message_id=random.randint(1, 2700))
+    elif update.message.from_user.username == "fochi_ip":
         if TIME_TO_REPOST.get(update.message.chat_id, 0) == 0:
-            bot.sendMessage(update.message.chat_id, text=fochi_replyes[random.randint(0, len(fochi_replyes) - 1)], reply_to_message_id=update.message.message_id)
+            bot.sendMessage(update.message.chat_id, text=fochi_replyes[random.randint(0, len(fochi_replyes) - 1)],
+                            reply_to_message_id=update.message.message_id)
             TIME_TO_REPOST[update.message.chat_id] = random.randint(10, 50)
         else:
             TIME_TO_REPOST[update.message.chat_id] -= 1
-    # elif username == "futurobot_tg":
-    #     bot.sendMessage(update.message.chat_id, text=update.message.to_dict())
-    #     if TIME_TO_REPOST.get(update.message.chat_id, 0) == 0:
-    #         bot.sendMessage(update.message.chat_id, text=fochi_replyes[random.randint(0, len(fochi_replyes) - 1)], reply_to_message_id=update.message.message_id)
-    #         TIME_TO_REPOST[update.message.chat_id] = random.randint(10, 50)
-    #     else:
-    #         TIME_TO_REPOST[update.message.chat_id] -= 1
 
 
 def error(bot, update, error):
